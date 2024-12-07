3539d3d926fd845030e3f246aec55f359dd95196
kyongmin-s <kei.tenkawa@gmail.com>
2024-12-07 14:51:19 +0900

    20241206_tsxを使ったFunctions_Framework_実行環境

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as ff from '@google-cloud/functions-framework'
 
 ff.http('helloGET', (req: ff.Request, res: ff.Response) => {
-  res.send(`Hello World!`);
+  res.send(`Hello World desu!`);
 });
