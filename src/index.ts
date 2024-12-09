c15a9473387cc92e269c4919f694a49a8a61a8d7
kyongmin-s <kei.tenkawa@gmail.com>
2024-12-09 10:05:31 +0900

    20241208_Parcel を使ったTypeScriptのビルド環境

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as ff from '@google-cloud/functions-framework'
+import { hoge } from '~/src/hoge/hoge.js'
 
 ff.http('helloGET', (req: ff.Request, res: ff.Response) => {
-  res.send(`Hello World desu!`);
+  const hogehoge = hoge();
+  console.log(hogehoge)
+  res.send(`Hello World!`);
 });
