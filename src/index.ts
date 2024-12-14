86665786ffe93cb9a8909ef2ab2897851bdf7b67
kyongmin-s <kei.tenkawa@gmail.com>
2024-12-14 18:10:23 +0900

    20241212_デプロイ_Cloud Run functions をデプロイする

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as ff from '@google-cloud/functions-framework'
+import type { HttpFunction } from "@google-cloud/functions-framework";
+
 import { hoge } from '@/hoge/hoge.js'
 
-ff.http('helloGET', (req: ff.Request, res: ff.Response) => {
+export const helloGET: HttpFunction =  (req: ff.Request, res: ff.Response) => {
   let hogehoge: string = hoge();
   console.log(hogehoge)
   res.send(`Hello World!`);
-});
+};
