eb734dedc1ea24bc1246b769d82f5eb0b144ee38
kyongmin-s <kei.tenkawa@gmail.com>
2024-12-15 19:46:50 +0900

    20241213_TerraformでCloud Run functions をデプロイする

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import * as ff from '@google-cloud/functions-framework'
 import type { HttpFunction } from "@google-cloud/functions-framework";
 
-import { hoge } from '@/hoge/hoge.js'
-
 export const helloGET: HttpFunction =  (req: ff.Request, res: ff.Response) => {
-  let hogehoge: string = hoge();
-  console.log(hogehoge)
-  res.send(`Hello World!`);
+  res.send(`Hello, World!`);
 };
